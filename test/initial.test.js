fbd63893e7533184076514653734d359b6b061fa
Ajay Gandhi <aag255@cornell.edu>
2019-05-16 12:15:11 -0400

    Add tests for constructor

--- test/initial.test.js
+++ test/initial.test.js
@@ -17,3 +17,24 @@ test("has empty initial state", () => {
     expect(playlist[key]).toEqual(INITIAL_STATE[key]);
   });
 });
+
+describe("constructor", () => {
+  test("takes in args", () => {
+    const playlist = new Playlist({
+      default_queue_items: 15,
+      shuffle: true,
+    });
+    Object.keys(playlist).forEach((key) => {
+      if (key === "default_queue_items") expect(playlist[key]).toEqual(15);
+      else if (key === "shuffle")        expect(playlist[key]).toEqual(true);
+      else                               expect(playlist[key]).toEqual(INITIAL_STATE[key]);
+    });
+  });
+
+  test("clones items", () => {
+    const items = [{ "foo": "bar" }];
+    const playlist = new Playlist({ items });
+    items[0].foo = "baz";
+    expect(playlist.items[0].foo).toEqual("bar");
+  });
+});
